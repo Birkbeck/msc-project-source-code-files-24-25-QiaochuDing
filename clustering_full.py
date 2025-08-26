b76d0dda46e909de20236d4ac730fc8d308ee69c
Qiaochu Ding <QiaoChuDing@mac.home>
2025-08-26 15:44:37 +0100

    load data and replace missing placeholders

--- clustering_full.py
+++ clustering_full.py
@@ -1,4 +1,4 @@
-# Import necessary libraries
+# Import libraries
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -8,3 +8,8 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import davies_bouldin_score
 from sklearn.decomposition import PCA
+
+# Load data and replace '-' with NaN
+full_data = pd.read_excel("/Users/QiaoChuDing_1/Desktop/Birkbeck/2024-2025/4. Project/msc-project-source-code-files-24-25-QiaochuDing/full_data.xlsx")
+full_data.replace("-", np.nan, inplace=True)
+print(full_data.head())
