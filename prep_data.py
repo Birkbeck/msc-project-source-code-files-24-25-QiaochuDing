e7ca2595f5ead8405906c491bdcae334d34d54f6
Qiaochu Ding <QiaoChuDing@mac.home>
2025-09-02 20:19:30 +0100

    prep script

---
+++ prep_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from scipy.spatial.distance import cdist
+import joblib
